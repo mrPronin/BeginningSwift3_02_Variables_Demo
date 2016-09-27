22dd0ba2cb18013e247ed96d3441541e3fc9395d
Alexander Pronin <pronin.alx@gmail.com>
2016-09-27 15:30:41 +0300

    Initial commit

---
+++ variabledemo.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var hello = "Hi"
+hello = "Hello World"
+
+var helloWorldFromBrian = "Hi"
+var helloWorldFrombrian = "Hello"
+
+// my comment goes here
+/*
+ adfa 
+ adsfasdfasdf
+ 
+ asdfasdf
+ asdfa
+ */
+
+var greeting = "Hello"
+var name = "Brian"
+
+var completedGreeting = greeting + name
+
+var myNumber = 10.5
+let 𝛑 = 3.141
+𝛑 * 2
+
+
